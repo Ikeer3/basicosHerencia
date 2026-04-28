2f64064c118f20285dd3b6ed7bc2e8351d822d5a
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-04-28 10:04:06 +0200

    Añadido atributo, getter y metodo hacerSonido

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,16 @@
 public class Animal {
-    
+
+    private String nombre;
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String hacerSonido() {
+        return " ";
+    }
 }
